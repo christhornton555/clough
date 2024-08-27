38d0995832c4603f206b20c57cbcff5aa3c49d08
Chris Thornton <cjthornton78@gmail.com>
2024-08-27 22:56:17 +0100

    A basic utility to prettify xml files

---
+++ utils/prettify_xml.py
@@ -0,0 +1,25 @@
+# Utility to prettify XML files to make them easier to read
+
+import xml.etree.ElementTree as ET
+import xml.dom.minidom
+
+def pretty_print_xml(input_file, output_file):
+    # Parse the XML file
+    tree = ET.parse(input_file)
+    root = tree.getroot()
+    
+    # Convert the ElementTree to a string
+    xml_str = ET.tostring(root, encoding='unicode')
+
+    # Use minidom to pretty print the XML string
+    dom = xml.dom.minidom.parseString(xml_str)
+    pretty_xml_as_string = dom.toprettyxml()
+    
+    # Write the pretty XML to the output file
+    with open(output_file, 'w') as file:
+        file.write(pretty_xml_as_string)
+
+# Just put the relative filepath here
+input_file = r'test_data\test_sheet_01 - Copy\[Content_Types].xml'
+output_file = input_file[:-4] + '_PRETTIFIED.xml'
+pretty_print_xml(input_file, output_file)
