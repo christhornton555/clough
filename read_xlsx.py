a4d4bdfa839285123af67455dc8b0d76bb428c0c
Chris Thornton <cjthornton78@gmail.com>
2024-08-28 00:52:48 +0100

    Reading XML of workbook metadata, bs4 not parsing it properly yet tho

--- read_xlsx.py
+++ read_xlsx.py
@@ -4,9 +4,10 @@ This script opens a basic .xlsx file, reads its contents, and displays them to s
 import shutil  # Various utilities including for copying files
 import os  # Handles things like deleting files
 from datetime import datetime  # For timestamping files
+from bs4 import BeautifulSoup  # For picking data out of an XML file - also requires lxml
 
 
-# Returns the current time as a string for timestamping filenames
+# Return the current time as a string for timestamping filenames
 # TODO - I might move this to its own module - I anticipate using it a lot
 def get_current_time_string():
     now = datetime.now()
@@ -14,25 +15,47 @@ def get_current_time_string():
     return formatted_time
 
 
-# Copies any xlsx file to a zip, which it then extracts
+# Copy any xlsx file to a zip, and then extract
 def unzip_xlsx_file(filename):
     if filename[-5:] == '.xlsx':
         current_time = get_current_time_string()
         converted_file = f'test_data/{current_time}_output.zip'
         shutil.copy(filename, converted_file)  # Create the zip file
 
-        archive_name = converted_file[:-4]
-        shutil.unpack_archive(converted_file, archive_name)  # Unzips to dir
+        archive_path = converted_file[:-4]
+        shutil.unpack_archive(converted_file, archive_path)  # Unzips to dir
 
         os.remove(converted_file)  # Get rid of the temp zip file
 
-        print(f'File unzipped to path {archive_name}')
+        print(f'File unzipped to path {archive_path}')
 
     else:
         print(f'Specified file, {filename}, was not a .xsls file.')
 
+    return archive_path
+
+
+# Read sheets info from XML
+def get_sheets_info(archive_path):
+    workbook_path = f'{archive_path}/xl/workbook.xml'
+    with open(workbook_path, 'r') as workbook_file:
+        workbook_xml_data = workbook_file.read()
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(workbook_xml_data, 'xml')
+
+    # Find all <s:sheet> tags
+    sheet_tags = soup.find_all('s:sheets')
+    print(f'sheet tags: {sheet_tags}')
+
+    # Extract name and sheetId values
+    for sheet in sheet_tags:
+        name = sheet.get('name')
+        sheet_id = sheet.get('sheetId')
+        print(f"Sheet Name: {name}, Sheet ID: {sheet_id}")
 
 
 if __name__ == '__main__':
     file_to_read = r'test_data\test_sheet_01.xlsx'
-    unzip_xlsx_file(file_to_read)
+    temp_archive_path = unzip_xlsx_file(file_to_read)
+    get_sheets_info(temp_archive_path)
