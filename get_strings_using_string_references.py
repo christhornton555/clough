50663ad228d546df4a4f1a93c375f9e5cd86b77b
Chris Thornton <cjthornton78@gmail.com>
2024-08-28 23:37:23 +0100

    First commit on this branch, set up basic file to find strings

---
+++ get_strings_using_string_references.py
@@ -0,0 +1,14 @@
+'''
+This script takes a string reference and find and returns the string it relates to
+'''
+def get_string(ref):
+    print(f'Find string {ref}')
+
+
+if __name__ == '__main__':
+    print('   --- START ---')
+
+    string_reference = 1
+    get_string(string_reference)
+
+    print('   --- END ---')
