a944e9551427763bd60a0851555db6a08547ebe9
Chris Thornton <cjthornton78@gmail.com>
2024-08-29 00:19:09 +0100

    Now returns a string from the temp archive by its reference number

--- get_strings_using_string_references.py
+++ get_strings_using_string_references.py
@@ -1,14 +1,26 @@
 '''
 This script takes a string reference and find and returns the string it relates to
 '''
-def get_string(ref):
-    print(f'Find string {ref}')
+from bs4 import BeautifulSoup  # For picking data out of an XML file 
+
+def get_string(ref, archive_path):
+    strings_file_path = f'{archive_path}/xl/sharedStrings.xml'
+    with open(strings_file_path, 'r') as strings_file_file:
+        strings_xml_data = strings_file_file.read()
+
+    # Create a BeautifulSoup object
+    strings_soup = BeautifulSoup(strings_xml_data, features='xml')
+
+    string_tags = strings_soup.find_all('si')
+
+    print(string_tags[ref].find('t').text)
 
 
 if __name__ == '__main__':
     print('   --- START ---')
 
-    string_reference = 1
-    get_string(string_reference)
+    string_reference = 0
+    temp_archive_path = 'test_data/output/20240829_000803_output'
+    get_string(string_reference, temp_archive_path)
 
     print('   --- END ---')
