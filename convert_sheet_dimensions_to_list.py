90d9a27386a43c6cac95996d194b1d76b22a1525
Chris Thornton <cjthornton78@gmail.com>
2024-08-29 08:32:45 +0100

    First commit of script to convert dimensions into a list of cell references to iterate through

---
+++ convert_sheet_dimensions_to_list.py
@@ -0,0 +1,16 @@
+
+def dimensions_to_list(dims):
+    start_ref = dims.split(':')[0]
+    end_ref = dims.split(':')[1]
+
+    print(start_ref, end_ref)
+
+
+if __name__ == '__main__':
+    print('   --- START ---')
+
+    dimensions = 'A1:B31'
+    dim_list = dimensions_to_list(dimensions)
+    print(dim_list)
+
+    print('   --- END ---')
