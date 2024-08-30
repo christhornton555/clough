470dde09050f960dfb2920e7ac8171d1a16b6f48
Chris Thornton <cjthornton78@gmail.com>
2024-08-31 00:44:03 +0100

    Now splits up any size dimensions into start/end columns then rows

--- convert_sheet_dimensions_to_list.py
+++ convert_sheet_dimensions_to_list.py
@@ -1,20 +1,23 @@
 
+def split_cell_reference_into_rows_and_columns(cell_reference):
+    for char in range(len(cell_reference)):
+        if not cell_reference[char].isalpha():
+            split_start_ref_at = char
+            break
+    return [cell_reference[:split_start_ref_at], cell_reference[split_start_ref_at:]]
+
+
 def dimensions_to_list(dims):
     start_ref = dims.split(':')[0]
     end_ref = dims.split(':')[1]
 
-    print(start_ref, end_ref)
-    column_letter = 'AA63'[0]
-    row_number = int('AA63'[1:])
-    column_index = ord(column_letter.upper()) - ord('A')
-    print(column_letter, row_number, column_index)
-
+    return[split_cell_reference_into_rows_and_columns(start_ref), split_cell_reference_into_rows_and_columns(end_ref)]
 
 
 if __name__ == '__main__':
     print('   --- START ---')
 
-    dimensions = 'A1:B31'
+    dimensions = 'ABC123:BDGJ31457'
     dim_list = dimensions_to_list(dimensions)
     print(dim_list)
 
