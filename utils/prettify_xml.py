eb5b8e3d9f52d3ec25620039f0fa2ef5408d7a10
Chris Thornton <cjthornton78@gmail.com>
2024-09-01 19:37:16 +0100

    Fixed an error with the prettify_xml script caused by mis-matched character encoding

--- utils/prettify_xml.py
+++ utils/prettify_xml.py
@@ -2,6 +2,7 @@
 
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
+from io import StringIO
 
 def pretty_print_xml(input_file, output_file):
     # Parse the XML file
@@ -9,17 +10,28 @@ def pretty_print_xml(input_file, output_file):
     root = tree.getroot()
     
     # Convert the ElementTree to a string
-    xml_str = ET.tostring(root, encoding='unicode')
+    xml_str = ET.tostring(root)
 
-    # Use minidom to pretty print the XML string
-    dom = xml.dom.minidom.parseString(xml_str)
+    # Use StringIO to create an in-memory file
+    in_mem_file = StringIO()
+    in_mem_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')  # XML declaration
+    in_mem_file.write(xml_str.decode('utf-8'))  # Write the actual XML content
+    in_mem_file.seek(0)  # Reset the cursor position
+
+    # Parse the in-memory XML string using minidom
+    dom = xml.dom.minidom.parseString(in_mem_file.read())
     pretty_xml_as_string = dom.toprettyxml()
     
     # Write the pretty XML to the output file
-    with open(output_file, 'w') as file:
+    with open(output_file, 'w', encoding='utf-8') as file:
         file.write(pretty_xml_as_string)
 
-# Just put the relative filepath here
-input_file = r'test_data\test_sheet_01 - Copy\xl\theme\theme1.xml'
-output_file = input_file[:-4] + '_PRETTIFIED.xml'
-pretty_print_xml(input_file, output_file)
+if __name__ == '__main__':
+    print('   --- START ---')
+
+    # Just put the relative filepath here
+    input_file = r'test_data/test_sheet_01 - Copy/xl/theme/theme1.xml'
+    output_file = input_file[:-4] + '_PRETTIFIED.xml'
+    pretty_print_xml(input_file, output_file)
+    
+    print('   --- END ---')
