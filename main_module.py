444be3bce67e7cf07834fb9140790d938149daf8
Chris Thornton <cjthornton78@gmail.com>
2024-09-02 23:28:52 +0100

    This now reads date formats (well, just one for now) from the various places and applies them. It works! For this one specific case!

--- main_module.py
+++ main_module.py
@@ -2,7 +2,7 @@
 This script calls all the other modules
 '''
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from get_raw_data_from_xlsx_sheets import unzip_xlsx_file, get_sheets_info, read_sheet_contents
 from get_strings_using_string_references import get_string
 from convert_sheet_dimensions_to_list import dimensions_to_list
@@ -36,7 +36,25 @@ if __name__ == '__main__':
             else:
                 if standard_num_formats_dict[get_style(int(sheet_contents[cell]['style_num']), temp_archive_path)][0] == 'General':
                     print(f'Cell {cell}: {sheet_contents[cell]['raw_value']} (style {sheet_contents[cell]['style_num']})')
+
+                elif standard_num_formats_dict[get_style(int(sheet_contents[cell]['style_num']), temp_archive_path)][0] == 'd-mmm-yy':
+                    # Excel processes dates idiosynchratically, so we can't just apply the number format without fixing some stuff
+                    # TODO - gonna need to apply this to all the other date formats I reckon, ugh. I might have half cracked it below, but need to check
+                    # Excel's date system starts on 1900-01-01
+                    # Subtract 1 because Excel incorrectly treats 1900 as a leap year
+                    base_date = datetime(1899, 12, 30)
+
+                    excel_date = int(sheet_contents[cell]['raw_value'])  # Date should be in Excel serial date format, e.g. 45123 = 16/07/2023
+
+                    # Convert the serial date to a datetime object
+                    converted_date = base_date + timedelta(days=excel_date)
+
+                    # Apply the desired format
+                    formatted_date = converted_date.strftime(standard_num_formats_dict[get_style(int(sheet_contents[cell]['style_num']), temp_archive_path)][1])
+                    
+                    print(f'Cell {cell}: {formatted_date} (style {sheet_contents[cell]['style_num']})')
+
                 else:
-                    print(f'Cell {cell}: {datetime.fromordinal(int(sheet_contents[cell]['raw_value'])).strftime(standard_num_formats_dict[get_style(int(sheet_contents[cell]['style_num']), temp_archive_path)][1])} (style {sheet_contents[cell]['style_num']}, i.e. numFmt {standard_num_formats_dict[get_style(int(sheet_contents[cell]['style_num']), temp_archive_path)][1]})')
+                    print(f'Cell {cell}: {sheet_contents[cell]['raw_value']} (style {sheet_contents[cell]['style_num']})')
 
     print('   --- END ---')
