8293fd64ac92b2b4811de54bc90e328479509895
Chris Thornton <cjthornton78@gmail.com>
2024-09-03 18:26:45 +0100

    HTML conversion script now accepting dict generated by reading workbook

--- main_module.py
+++ main_module.py
@@ -2,14 +2,14 @@
 This script calls all the other modules
 '''
 
-# from datetime import datetime
 from make_workbook_dict import make_workbook_dict
+from convert_data_to_html import convert_data_to_html
 
 if __name__ == '__main__':
     print('   --- START ---')
 
     file_to_read = r'test_data/test_sheet_01.xlsx'
     workbook_to_output = make_workbook_dict(file_to_read)
-    print(workbook_to_output)
+    convert_data_to_html(workbook_to_output)
 
     print('   --- END ---')
