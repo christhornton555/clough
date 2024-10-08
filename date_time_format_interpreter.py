9ed0be73d6179b7ac31d65719dd1e8779ee43625
Chris Thornton <cjthornton78@gmail.com>
2024-10-08 23:38:53 +0100

    I need to figure out how to process the months on their own before I add it into the other script. It's getting way too messy picking apart what Excel does.

---
+++ date_time_format_interpreter.py
@@ -0,0 +1,5 @@
+# Test file to figure out the flow control for interpreting custom datetime formats
+# The script apply_number_formatting.py is getting messy, so let's figure it out here
+
+formats = ['myhm','yyymms mmm','ddmmyy hhmmmss','yyymm', 'yyyymm - s mmm']
+
