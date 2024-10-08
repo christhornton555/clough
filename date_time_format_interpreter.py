fa48579114938f5e38d492b7a68e503403260da6
Chris Thornton <cjthornton78@gmail.com>
2024-10-09 00:50:59 +0100

    This is working correctly so far. Needs further testing

--- date_time_format_interpreter.py
+++ date_time_format_interpreter.py
@@ -1,42 +1,85 @@
-# Test file to figure out the flow control for interpreting custom datetime formats
-# The script apply_number_formatting.py is getting messy, so let's figure it out here
-
 import re
 
-dt_formats = ['myhm','yyymms mmm','ddmmyy hhmmmss','yyymm', 'yyyymm - s mmm', 'yyhhmm - s mmm ddmm', 'ddhhyyy', 'mmmmdddd']
+dt_formats = ['myhm',
+              'yyymms mmm',
+              'ddmmyy hhmmmss',
+              'yyymm',
+              'yyyymm - s mmm',
+              'yyhhmm - s mmm ddmm',
+              'ddhhyyy',
+              'mmmmdddd',
+              'hdmsm',
+              'yys mm  smm',
+              'mmyyh mm  smm m   - s']
 
 output_list = []
 
 for i in range(len(dt_formats)):
-    dt_format = dt_formats[i].lower()  # Excel is case insensitive, so just convert all to lower
-    if dt_format.count('m') > 0:  # Are there any 'm's present? TODO - what happens with upper case m's?
-        m_sequences = re.findall(r'm+', dt_format)
+    dt_format = dt_formats[i].lower()  # Excel is case insensitive
+    if dt_format.count('m') > 0:
+        m_sequences = list(re.finditer(r'm+', dt_format))
 
         seq_analysis_list = []
-        for seq in m_sequences:  # Sort out all the different sequence lengths
+        found_minute = False  # Track if a minute has been found
 
-            # Could be mins or months
-            if len(seq) == 1:
-                seq_analysis_list.append(f'{seq}: ??')
+        for seq in m_sequences:
+            seq_string = seq.group()
+            seq_start = seq.start()
+            seq_end = seq.end()
 
-            # Could be mins or months
-            elif len(seq) == 2:
-                seq_analysis_list.append(f'{seq}: ??')
+            # Could be minutes or months
+            if len(seq_string) == 1 or len(seq_string) == 2:
+                if not found_minute:
+                    # Check for 'h' or 's' preceding the 'm's
+                    is_minute = False
+                    idx = seq_start - 1
 
-            # All other lengths are always months
-            elif len(seq) == 3:  # Shortened month name
-                seq_analysis_list.append(f'{seq}: Jul')
-            elif len(seq) == 4:  # Full month name
-                seq_analysis_list.append(f'{seq}: July')
-            elif len(seq) == 5:  # Month initial only
-                seq_analysis_list.append(f'{seq}: J')
-            else:  # Default to full month name for any longer strings
-                seq_analysis_list.append(f'{seq}: default - July')
+                    while idx >= 0:
+                        char = dt_format[idx]
+                        if char.isalnum():
+                            if char in ('h', 's'):
+                                is_minute = True
+                            break
+                        idx -= 1
 
-        output_list.append(f'{i}. Sequences found: {seq_analysis_list}')
+                    if is_minute:
+                        seq_analysis_list.append(f'{seq_string}: Minute')
+                        found_minute = True
+                    else:
+                        seq_analysis_list.append(f'{seq_string}: Month')
+                else:
+                    # Default to months unless 's' is found after the 'm' sequence
+                    is_minute = False
+                    idx = seq_end
+                    while idx < len(dt_format):
+                        char = dt_format[idx]
+                        if char.isalnum():
+                            if char == 's':
+                                is_minute = True
+                            break
+                        idx += 1
 
-    else:  # No 'm' sequences are present
-        output_list.append(f'{i}. No sequences found')
+                    if is_minute:
+                        seq_analysis_list.append(f'{seq_string}: Minute')
+                        found_minute = True
+                    else:
+                        seq_analysis_list.append(f'{seq_string}: Month')
+                        
+            else:
+                # All other lengths are always months
+                if len(seq_string) == 3:
+                    seq_analysis_list.append(f'{seq_string}: Jul')
+                elif len(seq_string) == 4:
+                    seq_analysis_list.append(f'{seq_string}: July')
+                elif len(seq_string) == 5:
+                    seq_analysis_list.append(f'{seq_string}: J')
+                else:
+                    seq_analysis_list.append(f'{seq_string}: default - July')
+
+        output_list.append(f"{i}. Sequences found: {seq_analysis_list}")
+
+    else:
+        output_list.append(f"{i}. No 'm' sequences found")
 
 for statement in output_list:
     print(statement)
